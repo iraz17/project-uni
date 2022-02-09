373f946a39b6bfe8ddee8fc150467cb3181c62b0
iraz17 <brian.t17@protonmail.com>
2022-02-10 00:26:28 +0100

    partie front-end index bootstrap scss footer nav carrousel explain

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,16 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function home(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
-    }
-     #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        return $this->redirectToRoute('home');
     }
 }
